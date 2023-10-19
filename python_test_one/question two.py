683924213df449ed6dae1703813f02c26cd7c023
asaaba shallot <142730622+asaabaC@users.noreply.github.com>
2023-10-19 00:37:28 -0700

    Add files via upload
    python test

--- python_test_one/question two.py
+++ python_test_one/question two.py
@@ -17,17 +17,6 @@ num3 = float (22)
 min_number = min(num1,num2,num3)
 
 # Display the minimum number
-print(f"The minimum number is: {min_number}")
+print(f" minimum number : {min_number}")
 
 
-
-# Input three numbers from the user
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
-
-# Find the minimum number
-min_number = min(num1, num2, num3)
-
-# Display the minimum number
-print(f"The minimum number is: {min_number}")
